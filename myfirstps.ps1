1efabf45c54d38125fb755a035757f9d51da8243
Jason Chu <jcck2000@yahoo.com>
2023-10-15 22:40:10 -0700

    variable added

--- myfirstps.ps1
+++ myfirstps.ps1
@@ -3,3 +3,8 @@ Get-Process *win*
 get-date | Get-Member
 
 Find-Module -tag az
+
+# comments and notes for the script
+Write-Host "Hello world!"
+$Favcharacter = "spoongebob"
+$Favcharacter.GetType()
