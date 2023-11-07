dc96307c862c9056c402800032a36dd203e2045d
jcck2016 <jcck2000@yahoo.com>
2023-11-06 21:32:58 -0800

    added some more powercli scripts, vcheck,vcsa1

---
+++ PowerCLI/vcsa1.ps1
@@ -0,0 +1,16 @@
+Connect-VIServer -Server vcsa-CA-PROD1.contoso.com -Protocol https -User 'Administrator@vSphere.local'
+
+
+Get-VM
+Get-VM
+
+Get-VM -Name VM -Location Host01 | Move-VM –Destination Host02
+Get-VM -Name VM -Location Host01 | Move-VM –Destination Host02
+Get-VM -Name VM -Location Host01 | Move-VM –Destination Host02
+Get-VM -Name VM -Location Host01 | Move-VM –Destination Host02
+Get-VM -Name VM -Location Host01 | Move-VM –Destination Host02
+
+Get-Vmhost -name <FQDN of host> | Get-VirtualSwitch -Name  | New-VirtualPortgroup -Name "Name of PG" -VlanIDSS
+Get-Vmhost -name <FQDN of host> | Get-VirtualSwitch -Name  | New-VirtualPortgroup -Name "Name of PG" -VlanID
+Get-Vmhost -name <FQDN of host> | Get-VirtualSwitch -Name  | New-VirtualPortgroup -Name "Name of PG" -VlanID
+Get-Vmhost -name <FQDN of host> | Get-VirtualSwitch -Name  | New-VirtualPortgroup -Name "Name of PG" -VlanID
